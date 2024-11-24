700b40f4d251300f3ad38ab6bbed4022bcdadb01
Usu <usuedeaghe@gmail.com>
2024-11-24 18:54:46 +0000

    Dashboard Template set up

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import License
 
 # Create your views here.
+def dashboard(request):
+    licenses = License.objects.all()
+    context = {'licenses': licenses}
+    return render(request, 'core/dashboard.html', context)
