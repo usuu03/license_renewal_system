c680d7d331d4896ac621810ac2165dd80a1ca223
Usu <usuedeaghe@gmail.com>
2024-11-26 21:05:38 +0000

    changing to rest api format

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import License
-
-# Create your views here.
-def dashboard(request):
-    licenses = License.objects.all()
-    context = {'licenses': licenses}
-    return render(request, 'core/dashboard.html', context)
--- license_renewal_system/settings.py
+++ license_renewal_system/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'core.User'
+# AUTH_USER_MODEL = 'core.User'
 
 
 # Internationalization
